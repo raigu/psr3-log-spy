8a70ac14fbf5494568a54791c3d44e0fe6ba82cb
Rait Kapp <3442567+raigu@users.noreply.github.com>
2021-08-28 21:00:06 +0300

    Remove duplication
    by using abstract logger provided by psr/log package

--- src/LoggerSpy.php
+++ src/LoggerSpy.php
@@ -2,6 +2,7 @@
 
 namespace TestDouble\Psr3;
 
+use Psr\Log\AbstractLogger;
 use Psr\Log\LoggerInterface;
 use Psr\Log\LogLevel;
 use Psr\Log\NullLogger;
@@ -9,56 +10,16 @@ use Psr\Log\NullLogger;
 /**
  * I am a Logger. I spy on the caller. I can tell if caller has used me.
  */
-final class LoggerSpy implements LoggerInterface
+final class LoggerSpy extends AbstractLogger
 {
     private LoggerInterface $logger;
+
+    private array $logs;
 
     public function __construct(LoggerInterface $logger = null)
     {
         $this->logs = [];
         $this->logger = $logger ?? new NullLogger;
-    }
-
-    private array $logs;
-
-    public function emergency($message, array $context = array())
-    {
-        $this->log(LogLevel::EMERGENCY, $message, $context);
-    }
-
-    public function alert($message, array $context = array())
-    {
-        $this->log(LogLevel::ALERT, $message, $context);
-    }
-
-    public function critical($message, array $context = array())
-    {
-        $this->log(LogLevel::CRITICAL, $message, $context);
-    }
-
-    public function error($message, array $context = array())
-    {
-        $this->log(LogLevel::ERROR, $message, $context);
-    }
-
-    public function warning($message, array $context = array())
-    {
-        $this->log(LogLevel::WARNING, $message, $context);
-    }
-
-    public function notice($message, array $context = array())
-    {
-        $this->log(LogLevel::NOTICE, $message, $context);
-    }
-
-    public function info($message, array $context = array())
-    {
-        $this->log(LogLevel::INFO, $message, $context);
-    }
-
-    public function debug($message, array $context = array())
-    {
-        $this->log(LogLevel::DEBUG, $message, $context);
     }
 
     public function log($level, $message, array $context = array())
